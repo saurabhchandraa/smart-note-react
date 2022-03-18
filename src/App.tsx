177b94cbb3285cca6afebb1bcc55f0529ee84b67
Saurabh Chandra <saurabhchandraa@gmail.com>
2022-03-18 12:17:11 +0530

    hashrouter added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./components/Home";
 import { Layout } from "./components/Layout";
 import Login from "./components/Login/Login";
